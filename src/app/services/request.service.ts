e43c4fe3be1547137b70f5de61e34aabb00f0fde
waleedomega <waleedomega@hotmail.com>
2021-12-12 11:06:46 +0200

    request service

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { RequestParam } from '../shared/request-param';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+  public getRealTimeDepartures(data: any) {
+    const params: RequestParam = new RequestParam();
+    // Set query params
+    params.queryParams = {
+        key: 'e5f2da729a3c486a839f8d09e39ba950',
+        siteid: '1555',
+        timewindow: '60'
+    };
+    return this.apiService.callApi('getRealTimeDepartures', params, null, null);
+}
+
 }
